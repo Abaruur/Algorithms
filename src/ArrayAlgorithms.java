8583b7f3e506a01dd6c287945c98a37a93fb0b94
Abaruur <baruur.co@gmail.com>
2024-12-27 17:21:47 -0600

    Linear search using Java Streams to simplify implimentations

--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class ArrayAlgorithms {
     public static Integer linearSearch(int [] arr, int item) {
         for (int current : arr) {
@@ -9,7 +13,10 @@ public class ArrayAlgorithms {
     }
 
 
-
+// Linear search using java streams
+    public static OptionalInt linearSearch2(int [] arr, int item) {
+        return  Arrays.stream(arr).filter(x -> x == item).findFirst();
+    }
 
     public static void main(String[] args) {
         // Linear search arrays
@@ -17,7 +24,10 @@ public class ArrayAlgorithms {
 //        System.out.println(linearSearch(arr, 1));
 //        System.out.println(linearSearch(arr, 8));
 
-
+    // Linear search using Java Streams
+        int [] arr = {1, 2, 3, 4, 5, 6};
+        linearSearch2(arr, 1).ifPresent(System.out::println);
+        linearSearch2(arr, 8).ifPresent(System.out::println);
 
     }
 }
